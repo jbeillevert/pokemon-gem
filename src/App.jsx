bf0d0aac1b910e93a4719edf2f058f53eaa0fb74
J-E <beillevertj.contact@gmail.com>
2023-04-04 17:19:31 +0200

    Mise en place du style

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>blabla</h1>
+      <h2>Pokemon Gem</h2>
+      <h3>Bulbasaur</h3>
+      <p>Train this pokemon</p>
+      <button>⚒️</button>
+      
+    </div>
+  )
+}
+
+export default App
